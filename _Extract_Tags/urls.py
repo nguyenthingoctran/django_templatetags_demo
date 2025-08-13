d99d00bbe400556f1a733ec8390752271bc83672
Ngoc Tran <>
2025-08-13 17:34:31 +0700

    Create demo data table

--- _Extract_Tags/urls.py
+++ _Extract_Tags/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("table_demo/", views.index),
+    path("index/", views.index),
+    path("table_demo/", views.table_demo),
 ]
--- _Extract_Tags/views.py
+++ _Extract_Tags/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 def index(request):
     context = {"val": 10}
     return render(request, "base/main_layout.html", context)
+
+def table_demo(request):
+    data = {"data": 10}
+    return render(request, "app/templatetags/table_demo.html")
