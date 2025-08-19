ac0f321cb60a15c48a1e21e8c924f861e0b336d2
Ngoc Tran <>
2025-08-19 16:41:01 +0700

    Autoescape & Block

--- research_doc/views.py
+++ research_doc/views.py
@@ -5,7 +5,11 @@ from django.views.generic.base import TemplateView
 class Research_Doc(TemplateView):
     template_name = "app/research_doc/template_tags_and_filter.html"
 
+    def research_doc_autoescape(seft):
+      data = 'Hello &lt;i&gt;my&lt;/i&gt; World!'
+      return data
+      
     def get_context_data(self, **kwargs):
-        data = super().get_context_data(**kwargs)
-
-        return data
+      data = super().get_context_data(**kwargs)
+      data['autoescape'] = self.research_doc_autoescape()
+      return data
