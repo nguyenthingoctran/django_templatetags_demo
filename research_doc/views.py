2480e124948fe042f323a7253d1ba58d29ec5adb
Ngoc Tran <>
2025-08-20 17:32:54 +0700

    filter add

--- research_doc/views.py
+++ research_doc/views.py
@@ -5,21 +5,29 @@ from django.views.generic.base import TemplateView
 class Research_Doc(TemplateView):
     template_name = "app/research_doc/template_tags_and_filter.html"
 
-    def research_doc_autoescape(seft):
+    def research_doc_autoescape(self):
       data = 'Hello &lt;i&gt;my&lt;/i&gt; World!'
       return data
     
-    def research_doc_cycle(seft):
+    def research_doc_cycle(self):
       data = ['Apple', 'Banana', 'Cherry', 'Orange', 'Durian']
       return data
     
-    def research_doc_cycle_color(seft):
+    def research_doc_cycle_color(self):
       data = 'springgreen'
       return data
+    
+    def research_doc_filter_add(self):
+      context = {
+        'fruits': ['Apple', 'Banana', 'Cherry'],   
+        'vegetables': ['Asparagus', 'Broccoli', 'Carrot'],
+      }
+      return context
       
     def get_context_data(self, **kwargs):
       data = super().get_context_data(**kwargs)
       data['autoescape'] = self.research_doc_autoescape()
       data['cycle'] = self.research_doc_cycle()
       data['cycle_color'] = self.research_doc_cycle_color()
+      data['filter_add'] = self.research_doc_filter_add()
       return data
