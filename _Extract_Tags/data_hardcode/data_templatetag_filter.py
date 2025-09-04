51bf33f7f7f634904a8b03b550060cabb4c7654a
Ngoc Tran <>
2025-09-04 11:04:39 +0700

    upper

--- _Extract_Tags/data_hardcode/data_templatetag_filter.py
+++ _Extract_Tags/data_hardcode/data_templatetag_filter.py
@@ -432,6 +432,15 @@ class DataResearchTemplateTagFilter():
           'data': views.Research_Doc().research_doc_cycle(),
           'template_name': 'app/research_doc/template_tags_and_filter/content_templatetags_and_filter/filter_modal/unordered_list.html'
         }
+      },{
+        'filter_name': 'upper',
+        'description': "Trả về tất cả các ký tự được in hoa",
+        'id_modal': 'modalUpper',
+        'modal': {
+          'title': 'Filter - Upper',
+          'data': views.Research_Doc().research_doc_cycle(),
+          'template_name': 'app/research_doc/template_tags_and_filter/content_templatetags_and_filter/filter_modal/upper.html'
+        }
       }
     ]
     return data
