d40cd0f32c487886d45d89f4bfee9fb9870e2e4a
Ngoc Tran <>
2025-09-17 14:14:32 +0700

    Object (create, assign, define_properties, define_property)

--- research_doc/data/list_object.py
+++ research_doc/data/list_object.py
@@ -13,8 +13,18 @@ class ListObjectResearch():
     data = [
       {
         'data_bs_target': 'st_assign',
-        'name': 'Assign',
+        'name': 'assign()',
         'include_template': 'app/research_doc/javascript/list_js_content/object/assign.html',
+      },
+      {
+        'data_bs_target': 'st_create',
+        'name': 'create()',
+        'include_template': 'app/research_doc/javascript/list_js_content/object/create.html',
+      },
+      {
+        'data_bs_target': 'st_defineProperties',
+        'name': 'defineProperties()',
+        'include_template': 'app/research_doc/javascript/list_js_content/object/define_properties.html',
       }
     ]
     return data
