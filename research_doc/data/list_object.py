63bbbb514dd81e2f104cf6c9134549b2a1d2241e
Ngoc Tran <>
2025-09-19 11:00:15 +0700

    is Frozen

--- research_doc/data/list_object.py
+++ research_doc/data/list_object.py
@@ -75,6 +75,11 @@ class ListObjectResearch():
         'data_bs_target': 'st_isExtensible',
         'name': 'isExtensible()',
         'include_template': 'app/research_doc/javascript/list_js_content/object/isExtensible.html',
+      },
+      {
+        'data_bs_target': 'st_isFrozen',
+        'name': 'isFrozen()',
+        'include_template': 'app/research_doc/javascript/list_js_content/object/isFrozen.html',
       }
     ]
     return data
